7790b493181691044462ed6e0d149652e60f6e87
Dowo <jo.jo.zen501@gmail.com>
2025-08-31 17:53:49 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+import fondo from "./img/fondo.png";
+import fondo_dos from "./img/fondo_dos.png";
+
+const body = document.querySelector("body");
+body.style.backgroundImage = `url(${fondo_dos})`;
